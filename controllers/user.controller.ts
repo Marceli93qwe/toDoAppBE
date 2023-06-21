1bdad37bda145c8c10851a5088628336e2f38661
Marcel <zseg1ipg.mendrok.marcel@gmail.com>
2023-06-21 13:16:15 +0200

    updated signing token, now it takes secret key from .env file

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,9 +1,12 @@
 import {NextFunction, Request, Response} from "express";
-import {UserRecord} from "../records/user.record";
-import {NotFoundError, UnauthorizedError} from "../middlewares/error.middleware";
+import dotenv from "dotenv";
 import {sign} from "jsonwebtoken";
 import {compare} from "bcrypt";
+import {NotFoundError, UnauthorizedError} from "../middlewares/error.middleware";
+import {UserRecord} from "../records/user.record";
 
+
+dotenv.config()
 export const register = async (req: Request, res: Response, next: NextFunction) => {
     const {email, username, password} = req.body;
     const newUser = new UserRecord({username, email, password});
@@ -16,7 +19,7 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
     const user = await UserRecord.findByEmail(email);
     if (!user) throw new NotFoundError("couldn't find user with this email");
     if (await compare(password, user.password)) {
-        const token = sign({id: user.id}, "sekretKey");
+        const token = sign({id: user.id}, process.env.SECRET_KEY);
         return res.json({token})
     }
     throw new UnauthorizedError("Wrong password given");
