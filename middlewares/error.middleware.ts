8e8d55de8bc593b0b4a692755cf34327b5dcf066
Marcel <zseg1ipg.mendrok.marcel@gmail.com>
2023-06-21 13:03:32 +0200

    Added 3 new classes from Error, defining what error occurred in the program

--- middlewares/error.middleware.ts
+++ middlewares/error.middleware.ts
@@ -1,13 +1,31 @@
 import {NextFunction, Request, Response} from "express";
 
 export class ValidationError extends Error {
-};
+}
+
+export class NotFoundError extends Error {
+}
+
+export class UnauthorizedError extends Error {
+
+}
+
+export class ConflictError extends Error {
+}
 
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    if (err instanceof ValidationError) {
+    console.log(err);
+    const {message} = err;
+    if (err instanceof ValidationError || err instanceof NotFoundError) {
         res.status(404);
-        res.json({message: err.message});
+        res.json({message});
+    } else if (err instanceof ConflictError) {
+        res.status(409);
+        res.json({message});
+    } else if (err instanceof UnauthorizedError) {
+        res.status(401);
+        res.json({message});
     } else {
         res.status(500);
         res.json({message: 'Internal server error'});
