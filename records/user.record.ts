61ce414790a003616aae0c64ad3166e9f3966d8f
Marcel <zseg1ipg.mendrok.marcel@gmail.com>
2023-06-21 13:13:03 +0200

    added methods findByEmail and userExists to class UserRecord

--- records/user.record.ts
+++ records/user.record.ts
@@ -1,33 +1,55 @@
 import {pool} from "../config/db.config";
-import {ValidationError} from "../middlewares/error.middleware";
+import {ConflictError, ValidationError} from "../middlewares/error.middleware";
+import {v4 as uuid} from "uuid";
+import {RowDataPacket} from "mysql2";
+import {hash} from "bcrypt";
 
 export class UserRecord {
-    id: string;
+    id?: string;
     username: string;
     email: string;
     password: string;
 
-    constructor(id: string, username: string, email: string, password: string) {
-        this.validateFields();
+    constructor({id, username, email, password}: { id?: string, username: string, email: string, password: string }) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.password = password;
+        this.validateFields();
+    }
+
+    static async findByEmail(email: string): Promise<UserRecord | null> {
+        const query = 'SELECT * FROM users WHERE email = :email LIMIT 1';
+        const [rows] = await pool.execute(query, {email}) as RowDataPacket[];
+        if (rows.length > 0) {
+            const {id, email, username, password} = rows[0];
+            return new UserRecord({id, username, password, email});
+        } else {
+            return null;
+        }
+    }
+
+    async userExists(): Promise<boolean> {
+        const query = 'SELECT COUNT(*) as count FROM users WHERE email = ?';
+        const [rows] = await pool.execute(query, [this.email]) as RowDataPacket[];
+        return rows[0].count > 0;
     }
 
     async register() {
+        if (await this.userExists()) throw new ConflictError("This email is already taken");
         const query = `INSERT INTO users (id, username, email, password)
                    VALUES (:id, :username, :email, :password)`;
         const values = {
             id: this.id,
             username: this.username,
             email: this.email,
-            password: this.password,
+            password: await hash(this.password, 10),
         };
-        const [rows] = await pool.execute(query, values);
+        await pool.execute(query, values);
     }
 
     validateFields(): void {
+        if (!this.id) this.id = uuid();
         if (
             typeof this.id !== 'string' ||
             this.id.length !== 36 ||
