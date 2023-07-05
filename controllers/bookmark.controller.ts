b92582ac7c7623fd7365a47387bcc2ba6e4cbb5b
Marcel <zseg1ipg.mendrok.marcel@gmail.com>
2023-07-05 20:57:05 +0200

    implementation of user's bookmark search to api

--- controllers/bookmark.controller.ts
+++ controllers/bookmark.controller.ts
@@ -1,15 +1,16 @@
 import {Request, Response} from "express";
 import {UserRecord} from "../records/user.record";
 import {BookmarkRecord} from "../records/bookmark.record";
+import {NotFoundError} from "../middlewares/error.middleware";
 
-export const bookmarkAdd = async (req: Request, res: Response) => {
+export const addBookmark = async (req: Request, res: Response) => {
     // Retrieve bookmark data from the request body
     const {id, bookmarkName, user_id} = req.body;
 
 // Check if the user exists
     const user = await UserRecord.findById(user_id);
     if (!user) {
-        return res.status(404).json({error: 'User with the provided ID does not exist.'});
+        throw new NotFoundError('User with the provided ID does not exist.');
     }
 // Create a new instance of BookmarkRecord
     const bookmark = new BookmarkRecord({id, bookmarkName, user_id});
@@ -19,4 +20,22 @@ export const bookmarkAdd = async (req: Request, res: Response) => {
 
     return res.status(201).json({message: 'Bookmark successfully added.'});
 
+}
+
+export async function getUserBookmarks(
+    req: Request,
+    res: Response,
+): Promise<void> {
+    const {user_id} = req.params;
+
+    // Check if the user exists before retrieving the bookmarks
+    const user = await UserRecord.findById(user_id);
+    if (!user) {
+        throw new NotFoundError('User with the provided ID does not exist.')
+    }
+
+    // Retrieve user's bookmarks using findByUserId method
+    const bookmarks = await BookmarkRecord.findByUserId(user_id);
+
+    res.json({bookmarks});
 }
