30569af1b7023e04dd1b9c0b5ece779e112ac354
Marceli93qwe <109629658+Marceli93qwe@users.noreply.github.com>
2024-06-02 23:20:35 +0200

    Create sendMail.controller.ts

---
+++ controllers/sendMail.controller.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+
+export async function sendEmail(name: string, email: string, message: string): Promise<void> {
+    // Skonfiguruj transporter dla Nodemailer
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'your-email@gmail.com',
+            pass: 'your-email-password',
+        },
+    });
+
+    const mailOptions = {
+        from: email,
+        to: 'support@yourdomain.com',
+        subject: `Wiadomość wsparcia od ${name}`,
+        text: message,
+    };
+
+    // Wyślij e-mail
+    await transporter.sendMail(mailOptions);
+}
